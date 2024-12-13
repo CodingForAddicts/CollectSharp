79cb925682c02d6ad6ad8d3fd213446e6182f2fa
Antoine <bulletforaddicts@icloud.com>
2024-12-13 21:45:24 +0100

    fix typo

--- Woodstix/Program.cs
+++ Woodstix/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("All the tests have been move to Test.cs for better usage.");
+Console.WriteLine("All the tests have been moved to Test.cs for better usage.");
 Console.WriteLine("You can modify the test values to use the project!");
